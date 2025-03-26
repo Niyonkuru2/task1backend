04e880ca5b3a54310d71322165db083cca25eb4d
niyonkuru2 <niyonkurusam5@gmail.com>
2025-03-26 07:02:12 +0200

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //This ensures all routes start with "/api"
+  app.setGlobalPrefix('api');
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
